ac20748e76ff4317ec2ef9b1350712c8d5040f6c
Felix <111078933+fbicuul@users.noreply.github.com>
2022-09-14 23:28:04 +0100

    Prints the n times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,99 +1,50 @@
 #include "main.h"
 
-
-
 /**
+ * print_times_table - Prints a multiplication table up to param
  *
- *  * print_times_table - Prints a multiplication table up to param
- *
- *   *
- *
- *    * Return: Number matrix
- *
- *     */
-
+ * Return: Number matrix
+ */
 void print_times_table(int n)
-
 {
-
-		int x, y, z;
-
+	int x, y, z;
 
 
-
-
+	{
+		for (x = 0; x <= n; x++)
+		{
+			for (y = 0; y <= n; y++)
 			{
-
-						for (x = 0; x <= n; x++)
-
-									{
-
-													for (y = 0; y <= n; y++)
-
-																	{
-
-																						z = x * y;
-
-																										if (z > 99)
-
-																															{
-
-																																					_putchar(',');
-
-																																										_putchar(32);
-
-																																															_putchar((z / 100) + '0');
-
-																																																				_putchar(((z / 10) % 10) + '0');
-
-																																																									_putchar((z % 10) + '0');
-
-																																																													}
-
-																														else if (z > 9)
-
-																																			{
-
-																																									_putchar(',');
-
-																																														_putchar(32);
-
-																																																			_putchar(32);
-
-																																																								_putchar(((z / 10) % 10) + '0');
-
-																																																													_putchar((z % 10) + '0');
-
-																																																																	}
-
-																																		else
-
-																																							{
-
-																																													if (y != 0)
-
-																																																			{
-
-																																																										_putchar(',');
-
-																																																																_putchar(32);
-
-																																																																						_putchar(32);
-
-																																																																												_putchar(32);
-
-																																																																																	}
-
-																																																		_putchar(z + '0');
-
-																																																						}
-
-																																					}
-
-																_putchar('\n');
-
-																		}
-
-							}
-
+				z = x * y;
+				if (z > 99)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else if (z > 9)
+				{
+					_putchar(',');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
