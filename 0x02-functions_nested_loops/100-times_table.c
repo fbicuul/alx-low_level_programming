de6110354bf53dd24cef1f26be7e820250e4e245
Felix <111078933+fbicuul@users.noreply.github.com>
2022-09-14 23:36:32 +0100

    Prints the n times table starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints a multiplication table up to param
- *
+ * print_times_table - Prints a multiplication table
  * Return: Number matrix
  */
-void print_times_table(int n)
+
+void print_times_table (int n)
 {
 	int x, y, z;
 
