7c3b47049a5f113d14f950e6ad3f05f36a51d6fc
Felix <111078933+fbicuul@users.noreply.github.com>
2022-10-25 09:44:47 -0500

    List.H

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,26 +2,26 @@
 #define LISTS_H
 
 /**
- *  * File: lists.h
- *   * Auth: Felix Nwaora
- *    * Desc: Header file containing prototypes and definitions for all functions
- *     * and types written in the 0x12-more_singly_linked_lists directory.
- *      */
+ * File: lists.h
+ * Auth: Felix Nwaora
+ * Desc: Header file containing prototypes and definitions for all functions
+ * and types written in the 0x12-more_singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for Holberton project
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
 typedef struct listint_s
 {
-		int n;
-			struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
